9e9f0af0721ea857909dd6ffa59778b0fe50808b
annovcoer <annovcoer4de@gmail.com>
2026-05-22 23:02:33 +0700

    fixing midtrans snap redirect and callback

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -72,6 +72,8 @@ class CheckoutController extends Controller
         $user -> email = $data ['email'];
         $user -> name = $data ['name'];
         $user -> occupation = $data ['occupation'];
+        $user -> phone = $data ['phone'];
+        $user -> address = $data ['address'];
         $user ->save();
 
         // Create ke table Checkout
@@ -144,7 +146,7 @@ class CheckoutController extends Controller
 
         $checkout->midtrans_booking_code = $orderId;
         $transaction_details =[
-            '$order_id' => $orderId,
+            'order_id' => $orderId,
             'gross_amount' => $checkout->Camp->price * 1000
         ];
 
@@ -182,7 +184,7 @@ class CheckoutController extends Controller
 
         try {
             //get snap payment page URl
-            $paymentUrl =\Midtrans\Snap::createTransaction($params)->redirect_url;
+            $paymentUrl = \Midtrans\Snap::createTransaction($midtrans_params)->redirect_url;
             $checkout->midtrans_url = $paymentUrl;
             $checkout->save();
 
